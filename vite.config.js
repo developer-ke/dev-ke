1bf86e43172b981e91d9006b9cc56c142c9a3873
Develper-ke <antonytorotich@gmail.com>
2026-09-05 12:10:58 +0300

    final changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "https://developer-ke.github.io/dev-ke/",
   server: {
     port: 3000,
     open: true
